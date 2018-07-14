f15f2ad8a5fcd679fe25f6409fd8101e1bf3ea49
Ninjacop <tlutze@cannonschool.org>
2018-07-14 16:41:17 -0400

    Added some functions to help with Homebrew support

--- bin/ltz.rb
+++ bin/ltz.rb
@@ -1,17 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Ltz < Formula
-    desc "An all purpose, simple, lightweight CLT that helps create projects on the fly"
-    homepage "www.github.com/Ninjacop/homebrew-ltz"
-    url "https://github.com/Ninjacop/homebrew-ltz/files/2194878/ltz-1.0.1.zip"
-    sha256 "63a897eb8e371392f3b2c5f10ff0972a25884f3088d29b04964a09c019397575"
-    version = "1.0.1"
+  desc "An all purpose, simple, lightweight CLT that helps create projects on the fly"
+  homepage "www.github.com/Ninjacop/homebrew-ltz"
+  url "https://github.com/Ninjacop/homebrew-ltz/files/2194878/ltz-1.0.1.zip"
+  sha256 "63a897eb8e371392f3b2c5f10ff0972a25884f3088d29b04964a09c019397575"
+  # depends_on "cmake" => :build
 
-    depends_on "curl"
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
 
-    def install
-        bin.install "ltz"
-    end
-
-    test do 
-    end
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test ltz`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
 end
-     
