fd163bf136d8c9eb234fe70818642406c24699ab
Ninjacop <tlutze@cannonschool.org>
2018-07-14 16:47:11 -0400

    changed url and version

--- bin/ltz.rb
+++ bin/ltz.rb
@@ -4,8 +4,8 @@
 class Ltz < Formula
   desc "An all purpose, simple, lightweight CLT that helps create projects on the fly"
   homepage "www.github.com/Ninjacop/homebrew-ltz"
-  url "https://github.com/Ninjacop/homebrew-ltz/files/2194878/ltz-1.0.1.zip"
-  sha256 "63a897eb8e371392f3b2c5f10ff0972a25884f3088d29b04964a09c019397575"
+  url "https://github.com/Ninjacop/homebrew-ltz/files/2194878/ltz-1.0.1a.zip"
+  sha256 "4ba3890e19c3db97270d2c5afb52009402714c9516937e7bef0c2e4d6fba8197"
   # depends_on "cmake" => :build
 
   def install
