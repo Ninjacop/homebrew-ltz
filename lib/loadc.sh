993bf69690b94807f685bb9168954e67af12328f
Ninjacop <tlutze@cannonschool.org>
2018-07-15 22:45:19 -0400

    Added support for this language!

---
+++ lib/loadc.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+
+source $HOME/ltz/lib/loaddocs.sh
+
+function c-folders ()
+{
+    cd $HOME 
+    if [ ! -d $Name ]
+    then
+        mkdir bin
+        mkdir src 
+        mkdir tests
+        mkdir docs
+
+        cd src 
+        c-main
+        c-src-makefile
+        cd ..
+
+        cd tests 
+        c-tests-makefile
+        cd ..
+
+        cd docs 
+        intro $Name 
+        cd ..
+
+        c-gitignore
+        c-makefile
+        readme $Name
+        changelog
+    else 
+     echo "ERROR: Project folder \"$Name\" already exists. Delete the existing folder or make a new Project under a different name"
+    fi
+}
+
+function c-gitignore ()
+{
+    echo "# Object files
+    *.o
+    *.ko
+    *.obj
+    *.elf
+
+    # Precompiled Headers
+    *.gch
+    *.pch
+
+    # Libraries
+    *.lib
+    *.a
+    *.la
+    *.lo
+
+    # Shared objects (inc. Windows DLLs)
+    *.dll
+    *.so
+    *.so.*
+    *.dylib
+
+    # Executables
+    *.exe
+    *.out
+    *.app
+    *.i*86
+    *.x86_64
+    *.hex
+
+    # Debug files
+    *.dSYM/
+    *.su
+
+    # Own
+    *.p
+    *.sp
+    *.d
+    src/tmp
+    *.map" > .gitignore
+}
+
+function c-top-makefile ()
+{
+    echo " # IMPORTANT variables
+    PROGRAMNAME=$Name
+    SOURCES=main.c
+
+    #locations
+    SRCDIR=	src
+    OUTDIR=	bin
+    TESTDIR=tests
+
+    OBJECTS= \$(addprefix \$(SRCDIR)/, \$(SOURCES:.c=.o))
+    PROGRAM= \$(addprefix \$(OUTDIR)/, \$(PROGRAMNAME))
+
+    WARNINGS= -W -Wall -ansi -Wextra -pedantic -Wstrict-overflow=5 -Wshadow -Wpointer-arith -Wcast-qual -Wstrict-prototypes # turn on all possible warnings
+    COMPILER= -std=gnu89 # -s -march=native -flto -mtune=native -Os -Ofast -ffunction-sections -fdata-sections 		# strip, optimize for performance and then (mainly) for size. After that place all functions and data to separate sections
+    LINKER= # -flto -Wl,-Map=\$(PROGRAM).map,--cref,--gc-section -Wl,--build-id=none		# and with linker delete unneeded ones
+    DEBUG= -g
+
+    export SOURCES
+    export DEBUG
+    export COMPILER
+    export WARNINGS
+
+    all:\ $(PROGRAM)
+
+    .PHONY: objects clean
+    objects:
+        \$(MAKE) -C \$(SRCDIR) all
+
+    \$(PROGRAM): objects
+        cc -o \$@ \$(OBJECTS) \$(DEBUG) \$(LINKER)
+    #	strip -S --strip-all --remove-section=.note.gnu.gold-version --remove-section=.comment --remove-section=.note --remove-section=.note.gnu.build-id --remove-section=.note.ABI-tag $@
+
+    clean:
+        \$(MAKE) -C \$(SRCDIR) clean
+        \$(MAKE) -C \$(TESTDIR) clean
+        rm -f \$(PROGRAM) \$(PROGRAM).map" > Makefile
+}
+
+function c-src-makefile ()
+{
+    echo "# Variables. SOURCES is exported from makefile one folder up and so is DEBUG
+    OBJECTS=\$(SOURCES:.c=.o)
+    PROTO=\$(SOURCES:.c=.p)
+    STATPROTO=\$(SOURCES:.c=.sp)
+    DEPS=\$(SOURCES:.c=.d)
+
+    # Generate and include dependencies:
+
+    all: \$(DEPS) \$(OBJECTS)
+
+    %.o: %.c %.d
+        \$(CC) \$(WARNINGS) \$(DEBUG) \$(COMPILER) -c -o \$@ $<
+
+    # Generate dependencies
+    %.d: %.c
+        \$(CC) -MM -MG -MF \$@ $<
+
+    # Generate function prototypes
+    %.p: %.c
+        cproto $< > \$@
+
+    .PHONY: clean
+
+    clean:
+        rm -f \$(OBJECTS) \$(DEPS) \$(PROTO) \$(STATPROTO)
+
+    -include \$(DEPS)" > Makefile
+}
+
+function c-tests-makefile ()
+{
+    echo ".PHONY: clean
+
+    clean:" > Makefile
+}
+
+function c-main ()
+{
+    echo " " > main.c
+}
---
+++ lib/loadjava.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+source $HOME/ltz/bin/loaddocs.sh
+
+function java-folders ()
+{
+    if [[ ! -d $Name ]]
+    then
+        cd $HOME
+        mkdir $Name
+        cd $Name
+
+        mkdir -p src/main/java/com/$Name/app
+        mkdir -p src/main/resources
+        mkdir -p src/main/filters
+        mkdir -p src/main/webapp
+        mkdir -p src/test/resources
+        mkdir -p src/test/java/com/$Name/app
+        mkdir -p src/test/filters 
+        mkdir -p src/it  
+        mkdir -p src/assembly
+        mkdir -p src/site 
+        mkdir target
+
+        cd doc 
+        intro $Name 
+        cd ..
+
+        changelog
+        gpl3-license
+        java-gitignore
+        readme $Name
+        mkdir doc
+        java-pom
+
+    else
+        echo "ERROR: Project folder \"$Name\" already exists. Delete the existing folder or make a new Project under a different name"
+    fi
+}
+
+
+function java-pom ()
+{
+    echo "<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+    <modelVersion>4.0.0</modelVersion>
+    
+    <groupId>com.$Name.app</groupId>
+    <artifactId>$Name</artifactId>
+    <version>1.0-SNAPSHOT</version>
+    <packaging>jar</packaging>
+    
+    <name>Maven Quick Start Archetype</name>
+    <url>http://maven.apache.org</url>
+    
+    <dependencies>
+        <dependency>
+        <groupId>junit</groupId>
+        <artifactId>junit</artifactId>
+        <version>4.8.2</version>
+        <scope>test</scope>
+        </dependency>
+    </dependencies>
+    </project> " > pom.xml
+}
+
+
+function java-gitignore ()
+{
+    echo "# Compiled class file
+    *.class
+
+    # Log file
+    *.log
+
+    # BlueJ files
+    *.ctxt
+
+    # Mobile Tools for Java (J2ME)
+    .mtj.tmp/
+
+    # Package Files #
+    *.jar
+    *.war
+    *.nar
+    *.ear
+    *.zip
+    *.tar.gz
+    *.rar
+
+    # virtual machine crash logs, see http://www.java.com/en/download/help/error_hotspot.xml
+    hs_err_pid*" > .gitignore 
+}
