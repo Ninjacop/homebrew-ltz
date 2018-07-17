89caf239ea98df07b71008e03b14ff17f50ecda0
Ninjacop <tlutze@cannonschool.org>
2018-07-17 02:33:02 -0400

    Added SQL support!

---
+++ lib/loadsql.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+source $HOME/ltz/loaddocs.sh
+
+function sql-folders ()
+{
+        cd $HOME 
+    if [ ! -d $Name ]
+    then
+        mkdir tables
+        mkdir views 
+        mkdir stored-procedures
+        mkdir functions
+        mkdir synonyms
+        mkdir types
+        mkdir assemblies
+        mkdir doc
+
+        cd doc 
+        intro $Name 
+        cd ..
+
+        sql-gitignore
+        readme $Name
+        changelog
+        gpl3-license
+    else 
+     echo "ERROR: Project folder \"$Name\" already exists. Delete the existing folder or make a new Project under a different name"
+    fi
+}
+
+function sql-gitignore ()
+{
+    echo "
+    # Compiled source 
+    
+    *.com
+    *.class
+    *.dll
+    *.exe
+    *.o
+    *.so
+
+    # Packages 
+     
+    # it's better to unpack these files and commit the raw source
+    # git has its own built in compression methods
+    *.7z
+    *.dmg
+    *.gz
+    *.iso
+    *.jar
+    *.rar
+    *.tar
+    *.zip
+
+    # Logs and databases 
+    
+    *.log
+    *.sql
+    *.sqlite
+
+    # OS generated files 
+
+    .DS_Store
+    .DS_Store?
+    ._*
+    .Spotlight-V100
+    .Trashes
+    ehthumbs.db
+    Thumbs.db" > .gitignore
+}
